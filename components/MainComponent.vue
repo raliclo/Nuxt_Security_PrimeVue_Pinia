f7f217791ab93cd6786953d2bcb6a7c6a9de4de8
THSRC\b700381 <weishun_lo@thsrc.com.tw>
2025-01-08 13:38:27 +0800

    Add passthough example

--- components/MainComponent.vue
+++ components/MainComponent.vue
@@ -16,7 +16,9 @@ const tabs = {
 
 <template>
   <!-- With Dynamic Component -->
-  <Button @click="currentTab = 'ComponentOne'">What is HTML</Button>
+  <Button :pt="{
+    root: '!opacity-50',
+  }" @click="currentTab = 'ComponentOne'">What is HTML</Button>
   <Button @click="currentTab = 'ComponentTwo'">What is CSS</Button>
   <Button @click="currentTab = 'ComponentThree'">What is JS</Button>
 
